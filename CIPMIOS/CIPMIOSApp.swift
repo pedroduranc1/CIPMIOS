3667817ca0d01d82e53e40d3eb09626e071d3e47
pedroduranc1 <pedroduran2710@gmail.com>
2023-12-04 13:00:16 -0400

    Initial Commit

---
+++ CIPMIOS/CIPMIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CIPMIOSApp.swift
+//  CIPMIOS
+//
+//  Created by Pedro Duran on 4/12/23.
+//
+
+import SwiftUI
+
+@main
+struct CIPMIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CIPMIOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CIPMIOS
+//
+//  Created by Pedro Duran on 4/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
